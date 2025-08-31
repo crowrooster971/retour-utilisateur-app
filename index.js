18ae63f2e6aee0d947b1eb916829b5257bc60510
crowrooster971 <wanlishajna@hotmail.com>
2025-08-31 17:56:58 +0300

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const retours = require('./routes/retours');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/retour-utilisateur', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('/api/retours', retours);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
